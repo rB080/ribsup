edbc7008d8e36751bf0bf93f9c97620e6a6e1a87
Rajarshi Bhattacharya <rb080@narval1.narval.calcul.quebec>
2023-01-16 10:28:16 -0500

    engine changes for unpaired data

--- engine/make_translations.py
+++ engine/make_translations.py
@@ -1,8 +1,9 @@
 from net import translator
-from data import load_jsrt
+from data import load_jsrt, load_unpaired
 import torch
 from utils import translation_trainutils as TRANS
 import os.path as osp
+import os
 
 
 def run(args):
@@ -21,11 +22,41 @@ def run(args):
 
     
 
-    _, dataset_size, loader = load_jsrt.get_loader(
+    if args.dataset == "jsrt": 
+        _, dataset_size, loader = load_jsrt.get_loader(
         args, split="all", get_maps=True)
+        TRANS.save_translations(Gxy, Gyx, loader, dataset_size, device, osp.join(
+        args.base, args.workspace, "outputs"), args.use_attention)
+    else:
+        if osp.isdir(osp.join(
+        args.base, args.workspace, "outputs", "Train")) == False:
+            os.makedirs(osp.join(
+        args.base, args.workspace, "outputs", "Train"))
+            os.makedirs(osp.join(
+        args.base, args.workspace, "outputs", "Train", "X_to_Y"))
+            os.makedirs(osp.join(
+        args.base, args.workspace, "outputs", "Train", "Y_to_X"))
+
+        if osp.isdir(osp.join(
+        args.base, args.workspace, "outputs", "Test")) == False:
+            os.makedirs(osp.join(
+        args.base, args.workspace, "outputs", "Test"))
+            os.makedirs(osp.join(
+        args.base, args.workspace, "outputs", "Test", "X_to_Y"))
+            os.makedirs(osp.join(
+        args.base, args.workspace, "outputs", "Test", "Y_to_X"))
+
+        _, dataset_size, loader = load_unpaired.get_unpaired_loader(
+            args, split="train")
+        TRANS.save_unpaired_translations(Gxy, Gyx, loader, dataset_size, device, osp.join(
+        args.base, args.workspace, "outputs", "Train"), args.use_attention)
+
+        _, dataset_size, loader = load_unpaired.get_unpaired_loader(
+            args, split="test")
+        TRANS.save_unpaired_translations(Gxy, Gyx, loader, dataset_size, device, osp.join(
+        args.base, args.workspace, "outputs", "Test"), args.use_attention)
     
-    TRANS.save_translations(Gxy, Gyx, loader, dataset_size, device, osp.join(
-        args.base, args.workspace, "outputs"))
+    
     args.trans_batch = tb
     args.dataparallel = dp
     
--- engine/train_translators.py
+++ engine/train_translators.py
@@ -1,34 +1,47 @@
 from net import translator
-from data import load_jsrt
+from data import load_jsrt, load_unpaired
 import torch
 from utils import translation_trainutils as TRANS
 import os.path as osp
+import math
 
+def lr_controller(initial_lr, epoch, set_num_epoch=100, decay_factor=10):
+    ratio = math.pow(1/decay_factor, 1/(set_num_epoch-1))
+    LR_i = initial_lr * math.pow(ratio, epoch - 1)
+    return LR_i
 
 def run(args):
     device = torch.device(args.device)
     print("train_translators device:", device)
     Gxy, Gyx, Dx, Dy = translator.get_model_set(args)
-
-    optimizer_G = torch.optim.Adam(
-        params=list(Gxy.parameters()) + list(Gyx.parameters()), lr=args.trans_lr, weight_decay=0.0001)
-    optimizer_Dx = torch.optim.Adam(
-        params=Dx.parameters(), lr=args.trans_lr, weight_decay=0.0001)
-    optimizer_Dy = torch.optim.Adam(
-        params=Dy.parameters(), lr=args.trans_lr, weight_decay=0.0001)
     
-    _, train_dataset_size, train_loader = load_jsrt.get_loader(
-        args, split="train")
-    _, test_dataset_size, test_loader = load_jsrt.get_loader(
-        args, split="test")
+    if args.dataset == "jsrt":
+        _, train_dataset_size, train_loader = load_jsrt.get_loader(
+            args, split="train")
+        _, test_dataset_size, test_loader = load_jsrt.get_loader(
+            args, split="test")
+    else: 
+        _, train_dataset_size, train_loader = load_unpaired.get_loader(
+            args, split="train")
+        _, test_dataset_size, test_loader = load_unpaired.get_loader(
+            args, split="test")
     
     best_ssim_xy = 0
     best_ssim_yx = 0
     for epoch in range(1, args.trans_epochs+1):
+        LR = lr_controller(args.trans_lr, epoch, set_num_epoch=100, decay_factor=1.00001)
+        optimizer_G = torch.optim.Adam(
+        params=list(Gxy.parameters()) + list(Gyx.parameters()), lr=LR, weight_decay=0.0001)
+        optimizer_Dx = torch.optim.Adam(
+            params=Dx.parameters(), lr=LR, weight_decay=0.0001)
+        optimizer_Dy = torch.optim.Adam(
+            params=Dy.parameters(), lr=LR, weight_decay=0.0001)
+        print("Learning rate:", LR)
         D1, D2 = TRANS.train_one_epoch(epoch, Gxy, Gyx, Dx, Dy, train_loader, train_dataset_size,
-                                       optimizer_G, optimizer_Dx, optimizer_Dy, device, osp.join(args.base, args.workspace, "logs"))
+                                       optimizer_G, optimizer_Dx, optimizer_Dy, device, osp.join(args.base, args.workspace, "logs"), args.ssim_weight,
+                                       args.deep_supervision, args.use_attention)
         D1, D2 = TRANS.test_one_epoch(epoch, Gxy, Gyx, test_loader, test_dataset_size, device, osp.join(
-            args.base, args.workspace, "logs"))
+            args.base, args.workspace, "logs"), args.use_attention)
         torch.save(Gxy.state_dict(), osp.join(
             args.base, args.workspace, "saved_models", "Gxy_lm.pth"))
         torch.save(Gyx.state_dict(), osp.join(
